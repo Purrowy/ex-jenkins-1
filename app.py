731044269b7a519f308c9a43bb852404367dbc52
Purrowy <purr@pc.home>
2024-12-09 15:02:58 +0100

    app version 1.0

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    version = '1.0'
+    return f"Hello world ver {version}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
